1c4fd35162743b4b15a59961dc7d3ef7d7ef8ea2
Jason Mitchell <jason.mitchell.w@gmail.com>
2016-08-01 13:54:33 -0700

    Update fastboot-response.js

--- src/fastboot-response.js
+++ src/fastboot-response.js
@@ -3,6 +3,7 @@ var FastBootHeaders = require('./fastboot-headers');
 function FastbootResponse(response) {
   this.headers = new FastBootHeaders(response._headers);
   this.statusCode = 200;
+  this.locals = response.locals;
 }
 
 module.exports = FastbootResponse;
